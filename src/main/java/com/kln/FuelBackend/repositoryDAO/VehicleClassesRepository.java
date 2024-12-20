6276cca5a7d5f146451342077856f7c89fd862b8
thushan-123 <thushanmadusanka456@gmail.com>
2024-12-20 07:18:42 +0530

    add vehicleClassesRepository

--- src/main/java/com/kln/FuelBackend/repositoryDAO/VehicleClassesRepository.java
+++ src/main/java/com/kln/FuelBackend/repositoryDAO/VehicleClassesRepository.java
@@ -1,11 +1,9 @@
 package com.kln.FuelBackend.repositoryDAO;
 
-import com.kln.FuelBackend.entity.Vehicle;
+import com.kln.FuelBackend.entity.VehicleClasses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle , UUID> {
+public interface VehicleClassesRepository extends JpaRepository<VehicleClasses, Integer> {
 }
