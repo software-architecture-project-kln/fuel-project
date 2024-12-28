af6aac2bebf670a9e55a89e6c4c78665f7f5f753
thushan-123 <thushanmadusanka456@gmail.com>
2024-12-28 16:01:08 +0530

    create custom jpa sql query

--- src/main/java/com/kln/FuelBackend/repositoryDAO/UserRepository.java
+++ src/main/java/com/kln/FuelBackend/repositoryDAO/UserRepository.java
@@ -2,8 +2,14 @@ package com.kln.FuelBackend.repositoryDAO;
 
 import com.kln.FuelBackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query
+    Optional<User>  findByMobile(String mobile);
 }
