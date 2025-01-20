f4040a829f93351ae28e9dd3fa93b786d6504094
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-01-20 22:23:51 +0530

    add FuelStationReg component

---
+++ web-interface/src/pages/fuelStation/FuelStationReg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, Input } from 'antd';
+
+const FuelStationReg = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default FuelStationReg;
