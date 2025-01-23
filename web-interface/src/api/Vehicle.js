9d8a7e3512ac070a8d51ec5c6080ff4f02764019
parami1228 <paramianuththarajayamuni@gmail.com>
2025-01-23 11:22:55 +0530

    api call vehicle function develop

---
+++ web-interface/src/api/Vehicle.js
@@ -0,0 +1,141 @@
+import axios from "axios";
+import { createvehicleUri, createVehicleBusinessUri, findBusinessGovVehicleUri, findUserVehicleUri } from "./Uri";
+
+
+const createVehicle = async(
+    vehicleRegisterId,
+    vehicleEngineNo,
+    model,
+    yearOfManufacture,
+    OwnerId,
+    vehicleClassId,
+    fuelId,
+    token
+) => {
+
+    let data = JSON.stringify({
+        "vehicleRegisterId": vehicleRegisterId,
+        "vehicleEngineNo": vehicleEngineNo,
+        "model": model,
+        "yearOfManufacture": yearOfManufacture,
+        "ownerId": OwnerId,
+        "vehicleClassId": vehicleClassId,
+        "fuelId": fuelId
+    })
+
+    let config = {
+        "method": 'post',
+        "url": createvehicleUri,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        "data": data
+    }
+
+    try{
+        const response = await axios.request(config);
+        return response.data;
+    }catch (err){
+        console.log(err);
+        return null;
+    }
+
+}
+
+const createBusinessGovVehicle = async(
+    vehicleRegisterId,
+    vehicleEngineNo,
+    model,
+    yearOfManufacture,
+    OwnerId,
+    vehicleClassId,
+    fuelId,
+    token
+) => {
+
+    let data = JSON.stringify({
+        "vehicleRegisterId": vehicleRegisterId,
+        "vehicleEngineNo": vehicleEngineNo,
+        "model": model,
+        "yearOfManufacture": yearOfManufacture,
+        "ownerId": OwnerId,
+        "vehicleClassId": vehicleClassId,
+        "fuelId": fuelId
+    })
+
+    let config = {
+        "method": 'post',
+        "url": createVehicleBusinessUri,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        "data": data
+    }
+
+    try{
+        let response = await axios.request(config);
+        return response.data;
+    }catch (err){
+        console.log(err);
+        return null;
+    }
+
+}
+
+const findVehicleByBusinessId = async(businessId,token) => {
+
+    let url = `${findBusinessGovVehicleUri}/${businessId}`;
+
+    let config =  {
+        "method": 'get',
+        "url": url,
+        "headers": {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try{
+        let response = await axios.request(config);
+        return response.data;
+    }catch(e){
+        console.log(e);
+        return null;
+    }
+}
+
+const findVehicleByUserId = async(userId,token) => {
+    let url = `${findUserVehicleUri}/${userId}`;
+
+    let config = {
+        "method": 'get',
+        "url": url,
+        "headers": {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+            }
+
+    }
+
+    try{
+        let response = await axios.request(config);
+        return response.data;
+    }catch(err){
+        console.log(err);
+        return null;
+    }
+}
+
+
+
+
+
+
+export {
+    createVehicle,
+    createBusinessGovVehicle,
+    findVehicleByBusinessId,
+    findVehicleByUserId
+}
