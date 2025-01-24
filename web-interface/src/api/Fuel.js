4e6b8ed55e3274122c5efa31ae126b99a2dec815
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-01-24 23:32:09 +0530

    Add getAllFuel function✔️

---
+++ web-interface/src/api/Fuel.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { getAllFuellUri, updateFuelPriceUri } from "./Uri";
+
+
+const getAllFuel = async(token) => {
+
+    let config = {
+        "method": "GET",
+        "url": getAllFuellUri,
+        "headers": {
+            "Authorization": `Bearer ${token}`
+            }
+    
+    }
+
+    try{
+        const response = await axios.request(config);
+        return response.data;
+    }catch(error){
+        console.error(error);
+        return null;
+    }
+}
