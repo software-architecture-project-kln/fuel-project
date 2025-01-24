ceb32366164b39fe3df482232ab57aff7493cfda
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-01-24 23:33:25 +0530

    Add updateFuelPrice function✔️

--- web-interface/src/api/Fuel.js
+++ web-interface/src/api/Fuel.js
@@ -22,3 +22,29 @@ const getAllFuel = async(token) => {
         return null;
     }
 }
+
+const updateFuelPrice = async(administratorId,price,token) => {
+    let data = JSON.stringify({
+        "price": price
+    })
+
+    let url = `${updateFuelPriceUri}/${administratorId}`
+
+    let config = {
+        "method": "put",
+        "url": url,
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        "data": data
+    }
+
+    try {
+        const response = await axios.request(config);
+        return response.data;
+    }catch(error){
+        console.log(error);
+        return null
+    }
+}
