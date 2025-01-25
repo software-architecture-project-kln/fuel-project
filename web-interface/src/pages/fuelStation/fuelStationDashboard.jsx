40b411d16559e2633bb98982cc9c7a4043be9c7e
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-01-26 01:26:25 +0530

    Add form validation for employee creation✔️

---
+++ web-interface/src/pages/fuelStation/fuelStationDashboard.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Input } from "antd";
+import { toast, ToastContainer } from "react-toastify";
+import { employeeCreate } from "../../api/Employee";
+import { useNavigate } from "react-router-dom";
+import ShowEmployeeFuelStation from "../../components/employee/showEmployeeFuelStation";
+
+const FuelStationDashboard = () => {
+
+    const navigate = useNavigate();
+
+    const [token, setToken] = useState("");
+    const [fuelStation, setFuelStation] = useState({});
+
+    const [employeeUsername, setEmployeeUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [employeeEmail, setEmployeeEmail] = useState("");
+
+    const [error, setError] = useState("");
+    const [showModel, setShowModel] = useState(false);
+
+    const formValidation = () => {
+        setError(false);
+
+        if(employeeUsername === ""){
+            setError(true);
+            toast.error("employee username is required");
+            return;
+        }
+
+        if(password === ""){
+            setError(true);
+            toast.error("password is required");
+            return;
+        }
+
+        if(employeeEmail === ""){
+            setError(true);
+            toast.error("email is required");
+            return;
+        }
+    }
