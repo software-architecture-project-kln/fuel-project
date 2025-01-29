f3fa4eb942251bdb13431fdd45c7fe7bb91abd76
SandaruUOK <mihiranwee17@gmail.com>
2025-01-29 22:24:00 +0530

    fix the localstorage get data fuc err

--- web-interface/src/pages/UserProfile.jsx
+++ web-interface/src/pages/UserProfile.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { QRCode, Space, theme } from 'antd';
+import React, { useState, useEffect } from 'react';
+import {  Flex } from 'antd';
+import ShowUserRegVehicle from '../components/user/ShowUserRegVehicle';
+import UserLogout from '../components/user/UserLogout';
+import { Header } from 'antd/es/layout/layout';
+import '../style/UserProfile.css';
+import UserAccInfo from '../components/user/UserAccInfo';
+import { ToastContainer } from 'react-toastify';
 
 const UserProfile = () => {
+
+    const [token, setToken] = useState("");
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        setToken(localStorage.getItem("userAccessToken"));
+        const data = localStorage.getItem("userData");
+        setUserData(JSON.parse(data));
+    },[])
+
     return (
         <>
-        <h1>User Profile</h1>
+        <Header >
+            <Flex>
+                <h1>User Profile</h1>
+                {
+                    token && userData && (
+                        <UserAccInfo token={token} userData={userData} />
+                    )
+                }
+                <UserLogout />
+            </Flex>
+        </Header>
+        
+        <div>
+            {
+                token && userData && (
+                    <ShowUserRegVehicle 
+                        token={token}
+                        userId={userData.userId}
+                    />
+                )
+            }
+        </div>
+        <ToastContainer />
         </>
     )
 }
