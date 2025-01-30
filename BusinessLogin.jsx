6acbd1bb1a67091d880e261199c996f29b2f7f91
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-01-30 17:31:49 +0530

    implement bussiness login function

--- BusinessLogin.jsx
+++ BusinessLogin.jsx
@@ -6,3 +6,44 @@ import { ToastContainer, toast } from "react-toastify";
 import "../../style/BusinessLogin.css";
 
 
+const BusinessLogin = () => {
+
+    const navigate = useNavigate();
+
+    const [businessGovernmentRegNo, setBusinessGovernmentRegNo] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState();
+
+    const businessLogin = async() => {
+        setError(false);
+
+        if(businessGovernmentRegNo === ""){
+            toast.error("businessGov Reg number is required");
+            setError(true);
+            return
+        }
+
+        if(password === ""){
+            toast.error("Password is required");
+            setError(true);
+            return
+        }
+
+        if(!error){
+            const response = await businessGovAuthentication(businessGovernmentRegNo, password);
+            console.log(response);
+
+            if (response){
+                localStorage.setItem("businessAccessToken", response.accessToken);
+                localStorage.setItem("businessGovData",JSON.stringify(response.data));
+                navigate("/businessGov/dashboard");
+            }else{
+                toast.error("Invalid businessGov Reg number or password");
+                return
+            }
+        }
+    }
+
+    return (
+        <>
