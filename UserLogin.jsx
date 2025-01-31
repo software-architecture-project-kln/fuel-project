9f387c562c29a2b528b164baa64daaa0441b70ea
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-01-31 11:59:02 +0530

    implement the user login functionalty

--- UserLogin.jsx
+++ UserLogin.jsx
@@ -6,5 +6,36 @@ import { ToastContainer, toast } from "react-toastify";
 import "../style/UserLogin.css";  // Import the CSS file
 
 const UserLogin = () => {
+
+    const navigate = useNavigate();
+    const [mobile, setMobile] = useState("");
+    const [password, setPassword] = useState("");
+
+    const userLogin = async () => {
+        if (!mobile || !password) {
+            toast.error("Please enter both mobile number and password");
+            return;
+        }
+
+        try {
+
+            // Send data to the API
+            const res = await userAuthentication(mobile, password);
+            console.log(res);
+
+            if (res.statusCode === 200) {
+                toast.success("User logged in successfully");
+
+                localStorage.setItem("userAccessToken", res.accessToken);
+                localStorage.setItem("userData", JSON.stringify(res.data));
+
+                navigate("/vehicleRegister");
+            } else {
+                toast.error("Invalid username or password");
+            }
+        } catch (err) {
+            toast.error("API request failed");
+            console.error(err);
+        }
    
     };
