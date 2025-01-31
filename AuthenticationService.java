6485ea6091c9b0c9c328d8fa5d69f7d9153aa4b8
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-01-31 12:22:43 +0530

    constructor injection

--- AuthenticationService.java
+++ AuthenticationService.java
@@ -15,3 +15,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements AuthenticationServiceRepository{
+
+    private final UserRepository userRepository;
+
+    private final EmployeeRepository employeeRepository;
+
+    private final BusinessGovernmentRepository businessGovernmentRepository;
+
+    private final FuelStationRepository fuelStationRepository;
+
+    private final JwtUtility jwtUtility;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final AdministratorRepository administratorRepository;
+
+
+    public AuthenticationService(UserRepository userRepository, EmployeeRepository employeeRepository, BusinessGovernmentRepository businessGovernmentRepository, FuelStationRepository fuelStationRepository, JwtUtility jwtUtility, AuthenticationManager authenticationManager, AdministratorRepository administratorRepository) {
+        this.userRepository = userRepository;
+        this.employeeRepository = employeeRepository;
+        this.businessGovernmentRepository = businessGovernmentRepository;
+        this.fuelStationRepository = fuelStationRepository;
+        this.jwtUtility = jwtUtility;
+        this.authenticationManager = authenticationManager;
+        this.administratorRepository = administratorRepository;
+    }
